8c33e5476ec7a44606a2824fc91e2e95e7da5997
Vishwa1 <vishwajeet_vibhute@yahoo.com>
2015-10-14 16:11:11 +0100

    Selenium Grid
    Works with Firefox

---
+++ GridProject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Remote;
+using System.Collections.Generic;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Chrome;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+
+namespace SeleniumGrid
+{
+    [TestFixture]
+    public class Driver
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            //driver = new FirefoxDriver();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities = DesiredCapabilities.Firefox();
+            capabilities.SetCapability(CapabilityType.BrowserName, "firefox");
+            capabilities.SetCapability(CapabilityType.Platform, new Platform(PlatformType.Windows));
+            driver = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), capabilities);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://www.google.ie");
+        }
+    }
+}
